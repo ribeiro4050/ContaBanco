323dbc582c2f2f417f5dec6986d6538adc2f441c
ribeiro4050 <ribero4050@gmail.com>
2026-09-23 18:25:03 -0300

    feat: ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,12 @@
 public class ContaTerminal {
+    public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
 }
