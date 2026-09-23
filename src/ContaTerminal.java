a23c84d48baf30dbb058ac35e9fad9c475298419
ribeiro4050 <ribero4050@gmail.com>
2026-09-23 18:47:20 -0300

    feat: metodo main

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
         this.numero = numero;
@@ -9,4 +10,22 @@ public class ContaTerminal {
     private String agencia;
     private String nomeCliente;
     private double saldo;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da agencia\n");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o numero da conta\n");
+        int conta = scanner.nextInt();
+        System.out.println("Por favor, digite seu nome\n");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite seu saldo\n");
+        double saldo = scanner.nextDouble();
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta" +
+                "em nosso banco, sua agencia é " + agencia +" conta " + conta +
+                " com saldo R$ " + saldo + "ja esta disponivel para saque");
+    }
 }
+
+
